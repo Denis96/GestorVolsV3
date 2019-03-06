053354e0699c77701fe20201e4a3e90af712685d
Denis <m8.denis.pereira@gmail.com>
2019-03-06 21:35:00 +0100

    END();

--- src/persistencia/GestorXML.java
+++ src/persistencia/GestorXML.java
@@ -1,12 +1,21 @@
 package persistencia;
 
 import components.Avio;
+import components.Classe;
 import components.Ruta;
+import components.RutaIntercontinental;
+import components.RutaInternacional;
+import components.RutaNacional;
+import components.RutaTransoceanica;
+import components.TCP;
 import components.Tripulant;
+import components.TripulantCabina;
 import principal.Companyia;
 import principal.GestioVolsExcepcio;
 import principal.Vol;
 import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import nu.xom.*;
 import java.util.ArrayList;
 
@@ -15,7 +24,7 @@ import java.util.ArrayList;
  * @author cesca
  */
 public class GestorXML implements ProveedorPersistencia {
-        private Document doc;
+    private Document doc;
     private Companyia companyia;
 
     public Companyia getCompanyia() {
@@ -68,20 +77,137 @@ public class GestorXML implements ProveedorPersistencia {
 				if (pCompanyia.getComponents().get(i) instanceof Avio) {
 					
 					Element avio = new Element("avio");
-						////////////////////// AYUDEN-MEE
-						avio.addAttribute(new Attribute("codi", pCompanyia.getComponents().get(i).getCodi()));
+						avio.addAttribute( new Attribute( "codi", ( (Avio)pCompanyia.getComponents().get(i) ).getCodi() ) );
+						avio.addAttribute( new Attribute( "fabricant", ( (Avio) pCompanyia.getComponents().get(i)).getFabricant() ) );
+						avio.addAttribute( new Attribute( "model", ( (Avio) pCompanyia.getComponents().get(i)).getModel() ) );
+						avio.addAttribute( new Attribute( "capacitat", Integer.toString( ( (Avio) pCompanyia.getComponents().get(i)).getCapacitat() ) ) );
+						
+					for (int j = 0; j < ( (Avio)pCompanyia.getComponents().get(i) ).getClasses().size(); j++) {
+						Element classe = new Element("classe");
+							classe.addAttribute( new Attribute( "nom", (	(Classe)( (Avio)pCompanyia.getComponents().get(i) ).getClasses().get(j) ).getNom() ) );
+							classe.addAttribute( new Attribute( "capacitat", Integer.toString(	(	(Classe)( (Avio)pCompanyia.getComponents().get(i) ).getClasses().get(j) ).getCapacitat() ) ) );
+						
+						avio.appendChild(classe);
+					}
 					
+					companyia.appendChild(avio);
+
 				} else if (pCompanyia.getComponents().get(i) instanceof Ruta) {
+					Element ruta = new Element("");
+
+						Attribute codi = new Attribute( "codi" , ( (Ruta)pCompanyia.getComponents().get(i) ).getCodi() );
+						Attribute aeroportOri = new Attribute( "aeroportOri" , ( (Ruta)pCompanyia.getComponents().get(i) ).getAeroportOri() );
+						Attribute aeroportDes = new Attribute( "aeroportDes" , ( (Ruta)pCompanyia.getComponents().get(i) ).getAeroportDes() );
+						Attribute distancia = new Attribute( "distancia" , Double.toString( ( (Ruta)pCompanyia.getComponents().get(i) ).getDistancia() ) );
 					
+					if (pCompanyia.getComponents().get(i) instanceof RutaNacional ) {
+						ruta = new Element("rutaNacional");
+							Attribute pais = new Attribute( "pais" ,  ( (RutaNacional)pCompanyia.getComponents().get(i) ).getPais() );
+							
+							ruta.appendChild(codi);
+							ruta.appendChild(aeroportOri);
+							ruta.appendChild(aeroportDes);
+							ruta.appendChild(distancia);
+							ruta.appendChild(pais);
+							
+					} else if (pCompanyia.getComponents().get(i) instanceof RutaInternacional ) {
+						ruta = new Element("rutaInternacional");
+							Attribute paisOri = new Attribute( "paisOri" ,  ( (RutaInternacional)pCompanyia.getComponents().get(i) ).getPaisOri() );
+							Attribute paisDes = new Attribute( "paisDes" ,  ( (RutaInternacional)pCompanyia.getComponents().get(i) ).getPaisDes() ); 
+						
+							ruta.appendChild(codi);
+							ruta.appendChild(aeroportOri);
+							ruta.appendChild(aeroportDes);
+							ruta.appendChild(distancia);
+							ruta.appendChild(paisOri);
+							ruta.appendChild(paisDes);
+							
+					} else if (pCompanyia.getComponents().get(i) instanceof RutaIntercontinental ) {
+						ruta = new Element("rutaIntercontinental");
+							Attribute paisOri = new Attribute( "paisOri" ,  ( (RutaIntercontinental)pCompanyia.getComponents().get(i) ).getPaisOri() );
+							Attribute paisDes = new Attribute( "paisDes" ,  ( (RutaIntercontinental)pCompanyia.getComponents().get(i) ).getPaisDes() ); 
+							Attribute continentOri = new Attribute( "continentOri" ,  ( (RutaIntercontinental)pCompanyia.getComponents().get(i) ).getContinentOri() );
+							Attribute continentDes = new Attribute( "continentDes" ,  ( (RutaIntercontinental)pCompanyia.getComponents().get(i) ).getContinentDes() ); 
+						
+							ruta.appendChild(codi);
+							ruta.appendChild(aeroportOri);
+							ruta.appendChild(aeroportDes);
+							ruta.appendChild(distancia);
+							ruta.appendChild(paisOri);
+							ruta.appendChild(paisDes);
+							ruta.appendChild(continentOri);
+							ruta.appendChild(continentDes);
+							
+					} else if (pCompanyia.getComponents().get(i) instanceof RutaTransoceanica ) {
+						ruta = new Element("rutaTransoceanica");
+							Attribute paisOri = new Attribute( "paisOri" ,  ( (RutaTransoceanica)pCompanyia.getComponents().get(i) ).getPaisOri() );
+							Attribute paisDes = new Attribute( "paisDes" ,  ( (RutaTransoceanica)pCompanyia.getComponents().get(i) ).getPaisDes() ); 
+							Attribute continentOri = new Attribute( "continentOri" ,  ( (RutaTransoceanica)pCompanyia.getComponents().get(i) ).getContinentOri() );
+							Attribute continentDes = new Attribute( "continentDes" ,  ( (RutaTransoceanica)pCompanyia.getComponents().get(i) ).getContinentDes() ); 
+							Attribute ocea = new Attribute( "ocea" ,  ( (RutaTransoceanica)pCompanyia.getComponents().get(i) ).getOcea() ); 
+						
+							ruta.appendChild(codi);
+							ruta.appendChild(aeroportOri);
+							ruta.appendChild(aeroportDes);
+							ruta.appendChild(distancia);
+							ruta.appendChild(paisOri);
+							ruta.appendChild(paisDes);
+							ruta.appendChild(continentOri);
+							ruta.appendChild(continentDes);
+							ruta.appendChild(ocea);
+					}
+					
+					companyia.appendChild(ruta);
+
 				} else if (pCompanyia.getComponents().get(i) instanceof Tripulant) {
+					Element tripulant = new Element("");
+
+						Attribute passaport = new Attribute( "passaport" , ( (Tripulant)pCompanyia.getComponents().get(i) ).getPassaport() );
+						Attribute nom = new Attribute( "nom" , ( (Tripulant)pCompanyia.getComponents().get(i) ).getNom() );
+						Attribute edat = new Attribute( "edat" , Integer.toString( ( (Tripulant)pCompanyia.getComponents().get(i) ).getEdat() ) );
+						
+						String pattern = "MM-dd-yyyy";
+						DateFormat df = new SimpleDateFormat(pattern);
+						
+						Attribute dataAlta = new Attribute( "dataAlta" , df.format( ( (Tripulant)pCompanyia.getComponents().get(i) ).getDataAlta() ) );
+						Attribute horesVol = new Attribute( "horesVol" , Integer.toString( ( (Tripulant)pCompanyia.getComponents().get(i) ).getHoresVol() ) );
+						Attribute rang = new Attribute( "rang" , ( (Tripulant)pCompanyia.getComponents().get(i) ).getRang() );
+					
+					if (pCompanyia.getComponents().get(i) instanceof TCP) {
+							
+						tripulant = new Element("TCP");	
+							tripulant.appendChild(passaport);
+							tripulant.appendChild(nom);
+							tripulant.appendChild(edat);
+							tripulant.appendChild(dataAlta);
+							tripulant.appendChild(horesVol);
+							tripulant.appendChild(rang);
+					
+					} else if (pCompanyia.getComponents().get(i) instanceof TripulantCabina) {
+							
+						tripulant = new Element("tripulantCabina");	
+						
+						Attribute barres = new Attribute( "barres" , Integer.toString( ( (TripulantCabina)pCompanyia.getComponents().get(i) ).getBarres() ) );
+						
+							tripulant.appendChild(passaport);
+							tripulant.appendChild(nom);
+							tripulant.appendChild(edat);
+							tripulant.appendChild(dataAlta);
+							tripulant.appendChild(horesVol);
+							tripulant.appendChild(rang);
+							tripulant.appendChild(barres);
+
+					}
+					
+					companyia.appendChild(tripulant);
 					
 				} else if (pCompanyia.getComponents().get(i) instanceof Vol) {
 					
 				}
 				
-				
 			}
-        
+			
+        doc = new Document(companyia);
     }
 
     private void desarModel(String rutaFitxer) throws GestioVolsExcepcio {
